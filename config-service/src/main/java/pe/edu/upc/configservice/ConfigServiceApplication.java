4ab56a1dc8f59b2c2c6cb5c6f83b887f6bd577c0
renato-17 <renato.avela.17@gmail.com>
2021-09-28 19:59:55 -0500

    config service created

---
+++ config-service/src/main/java/pe/edu/upc/configservice/ConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+
+}
