b7dc9c79f0fcb1ba975bfca184274b46b36272cb
renato-17 <renato.avela.17@gmail.com>
2021-09-28 20:26:32 -0500

    admin service created

---
+++ admin-service/src/test/java/pe/edu/upc/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
